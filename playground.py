58232058567882b47d393a32d75c001402f12cdd
brylimz <superuser@brylimz.com>
2022-01-11 10:37:46 +0700

    practice

--- playground.py
+++ playground.py
@@ -1,8 +1,54 @@
 from bs4 import BeautifulSoup
-import lxml
-with open("website.html") as file:
-    contents = file.read()
+import requests
+response = requests.get("https://news.ycombinator.com/news")
+yc_web_page = response.text
 
-soup = BeautifulSoup(contents, "html.parser")
-print(soup)
+soup = BeautifulSoup(yc_web_page, "html.parser")
+articles = soup.find_all(name="a", class_="titlelink")
+articles_texts = []
+articles_links = []
 
+for article_tag in articles:
+    text = article_tag.getText()
+    articles_texts.append(text)
+    link = article_tag.get("href")
+    articles_links.append(link)
+    articles_upvotes = [score.getText() for score in soup.find_all(name="span", class_="score")]
+
+print(articles_texts)
+print(articles_links)
+print(articles_upvotes)
+
+
+
+
+
+
+
+
+
+# with open("website.html", encoding="utf8") as file:
+#     contents = file.read()
+#
+# soup = BeautifulSoup(contents, "html.parser")
+# # print(soup.prettify())
+# all_anchor_tags = soup.find_all(name="a")
+# print(all_anchor_tags)
+#
+# for tag in all_anchor_tags:
+#     # print(tag.getText())
+#     print(tag.get("href"))
+# #
+# # heading = soup.find(name="h1", id="name")
+# # print(heading)
+# #
+# # section_heading = soup.find(name="h3", class_="heading")
+# # print(section_heading.getText("name"))
+# #
+# # company_url = soup.select_one(selector="p a")
+# # print(company_url)
+#
+# # name = soup.select_one("#name")
+# # print(name)
+# headings = soup.select(".heading")
+# print(headings)
